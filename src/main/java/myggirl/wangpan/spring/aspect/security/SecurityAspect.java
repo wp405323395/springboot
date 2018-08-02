d7504030e4d49dc5d7169b9c796429adc9859ff4
max_wang <405323395@qq.com>
2018-08-02 09:48:02 +0800

    修改一个log的输出方式

--- src/main/java/myggirl/wangpan/spring/aspect/security/SecurityAspect.java
+++ src/main/java/myggirl/wangpan/spring/aspect/security/SecurityAspect.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,7 @@ import java.lang.reflect.Method;
 @Aspect
 @Component
 public class SecurityAspect {
+    private final static Logger logger = LoggerFactory.getLogger(SecurityAspect.class);
     @Autowired
     AuthService authService;
 
@@ -24,7 +27,7 @@ public class SecurityAspect {
     public void check(JoinPoint joinPoint){
         MethodSignature signature =  (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
-        System.out.println("拦截调用的方法名称是->"+method.getName());
+        logger.info("Aspect拦截调用的方法名称是=>{}",method.getName());
         if("girlList".equals(method.getName())||
                 "err".equals(method.getName())||
                 "findOne".equals(method.getName())||
