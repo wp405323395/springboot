bc45aeb94ed22f4167a4cfc45c07a2b2f76fd796
max_wang <405323395@qq.com>
2018-08-16 09:19:14 +0800

    项目进度

--- src/main/java/myggirl/wangpan/controller/GirlController.java
+++ src/main/java/myggirl/wangpan/controller/GirlController.java
@@ -14,50 +14,51 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@Api(value = "/school", tags="测试接口模块")
+@Api(value = "/school", tags="女孩儿的接口")
 @RestController
 @RequestMapping("/school")
 public class GirlController {
     @Autowired
     private GirlService girlService;
 
-    @ApiOperation(value="展示首页信息", notes = "展示首页信息")
+    @ApiOperation(value="搜索所有女孩儿的信息", notes = "拉取到所有女孩儿的数据。。。")
     @GetMapping(value = "/girls")
     public Result<List<Girl>> girlList(){
         return ResultUtil.success(girlService.girlList());
     }
 
+    @ApiOperation(value="随便获取一个字符串", notes = "")
     @GetMapping(value = "/getMsg")
     public Result<String> getMsg(){
         return ResultUtil.success("ffffffffffffffffffffffffffffffff");
     }
 
+    @ApiOperation(value="ControllerAdvice去拦截异常", notes = "")
     @GetMapping(value = "/err")
     public void err(){
         int a[] = {};
         a[1] = 10;
     }
 
+    @ApiOperation(value="赤裸裸的抛出业务异常", notes = "")
     @GetMapping(value = "/err2")
     public void err2(){
         throw new BusinessException(ResultEnum.PRIMARRY_SCHOOL);
     }
 
+    @ApiOperation(value="jpa的查询方式", notes = "")
     @PostMapping(value = "/findByAge")
     public Result<List<Girl>> findByAge(@RequestParam("age") Integer age) {
         return ResultUtil.success(girlService.findByAge(age));
     }
 
+    @ApiOperation(value="jpa的默认查询方式", notes = "")
     @PostMapping(value = "/findOne")
     public Result<List<Girl>> findOne(@RequestParam("id") Integer id) {
         return ResultUtil.success(girlService.findOne(id));
     }
 
-    /**
-     * 这里是用form表单形式传递数据的。
-     * @param girl
-     * @return
-     */
+    @ApiOperation(value="springboot的异常判断，女孩儿年龄不能小于10岁", notes = "")
     @PostMapping(value = "addGirl")
     public Result<Girl> addGirl(@Valid @RequestBody Girl girl, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
@@ -66,10 +67,7 @@ public class GirlController {
         return ResultUtil.success(girlService.addGirl(girl));
     }
 
-    /**
-     *
-     * @return
-     */
+    @ApiOperation(value="restfulApi形式", notes = "")
     @PostMapping(value = "/addGirlRestFul/age/{age}/cupSize/{cupSize}")
     public Result<Girl> addGirl(@PathVariable Integer age, @PathVariable String cupSize) {
         Girl girl = new Girl();
